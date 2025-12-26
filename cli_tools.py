646dba7744b133891d1efe7f080d69dddf82e3bd
martin <rmsk2@gmx.de>
2025-12-26 11:03:00 +0100

    Refactor

---
+++ cli_tools.py
@@ -0,0 +1,53 @@
+import pygame
+import uidfactory
+import collections
+import cardy
+from nfcplaylistconsts import *
+
+
+def wait_for_card(event_insert):
+    card_id = None
+
+    while card_id == None:
+        event = pygame.event.wait()
+        if event.type == event_insert:
+            card_id = event.card_id
+    
+    return card_id 
+
+
+# Ok let's massively overengineer this ;-)
+class Deferrer:
+    def __init__(self):
+        self._deferred = collections.deque()
+
+    def defer(self, f):
+        self._deferred.appendleft(f)
+
+    def unwind(self):
+        for f in self._deferred:
+            f()
+
+
+def main(payload_func):
+    try:
+        deferrer = Deferrer()
+
+        pygame.init()
+        deferrer.defer(pygame.quit)
+        event_insert = pygame.event.custom_type()
+        event_remove = pygame.event.custom_type()
+        event_err_generic = pygame.event.custom_type()
+        event_first_card = pygame.event.custom_type()
+
+        card_manager = cardy.CardManager(ALL_ATRS, uidfactory.UidReaderRepo(), event_insert, event_remove, event_err_generic, event_first_card)
+        card_manager.start()
+        deferrer.defer(card_manager.destroy)
+
+        payload_func(event_insert)        
+    except KeyboardInterrupt:
+        print()
+    except Exception as e:
+        print(e)
+    finally:
+        deferrer.unwind()
--- create_list.py
+++ create_list.py
@@ -4,11 +4,8 @@ from os import listdir
 from os.path import isfile, join
 from pathlib import Path
 import playlist
-import cardy
 from nfcplaylistconsts import *
-import pygame
-import uidfactory
-import collections
+import cli_tools
 
 
 def gen_listing(src_path):
@@ -16,6 +13,7 @@ def gen_listing(src_path):
     only_files.sort()
 
     return only_files
+
 
 def create_new_playlist(dir_to_list, out_name, card_id, playlist_name):
     data_dir = Path(dir_to_list)    
@@ -25,62 +23,27 @@ def create_new_playlist(dir_to_list, out_name, card_id, playlist_name):
     play_list.play_list = playlist_name
     play_list.data_dir = str(data_dir)
 
-    play_list.save()
+    play_list.save()   
 
-def main(playlist_dir, out_name, event_insert):
+
+def make_playlist(playlist_dir, out_name, event_insert):
     playlist_name = input("Enter playlist name          : ")
     sys.stdout.write("Place playlist card on reader: ")
     sys.stdout.flush()
-    card_id = None
 
-    while card_id == None:
-        event = pygame.event.wait()
-        if event.type == event_insert:
-            card_id = event.card_id
-            print(f"Card id {card_id}")
-
+    card_id = cli_tools.wait_for_card(event_insert)
+    print(f"Card id {card_id}")
     create_new_playlist(playlist_dir, out_name, card_id, playlist_name)
-
-
-# Ok let's massively overengineer this ;-)
-class Deferrer:
-    def __init__(self):
-        self._deferred = collections.deque()
-
-    def defer(self, f):
-        self._deferred.appendleft(f)
-
-    def unwind(self):
-        for f in self._deferred:
-            f()
+    print("\nNew playlist successfully created")
 
 
 if __name__ == "__main__":
-    try:
-        os.system(CLEAR_COMMAND)
-        deferrer = Deferrer()
-        
-        if len(sys.argv) < 3:
-            print("usage: create_list <dir to list> <new playlist file>")
-            sys.exit(42)
+    os.system(CLEAR_COMMAND)
+    
+    if len(sys.argv) < 3:
+        print("usage: create_list <dir to list> <new playlist file>")
+        sys.exit(42)
 
-        pygame.init()
-        deferrer.defer(pygame.quit)
-        event_insert = pygame.event.custom_type()
-        event_remove = pygame.event.custom_type()
-        event_err_generic = pygame.event.custom_type()
-        event_first_card = pygame.event.custom_type()
+    cli_tools.main(lambda x: make_playlist(sys.argv[1], sys.argv[2], x))
 
-        card_manager = cardy.CardManager(ALL_ATRS, uidfactory.UidReaderRepo(), event_insert, event_remove, event_err_generic, event_first_card)
-        card_manager.start()
-        deferrer.defer(card_manager.destroy)
-
-        main(sys.argv[1], sys.argv[2], event_insert)
-        print("\nNew playlist successfully created")
-    except KeyboardInterrupt:
-        print()
-    except Exception as e:
-        print(e)
-    finally:
-        deferrer.unwind()
         
